37fa8a12ab2ad07168004969a04bfffb00111ef3
Pratham943 <phoneixwasfound69@gmail.com>
2025-05-23 17:10:27 +0545

    Play message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className="bg-[#70EE9C] h-full">
-      <button className="btn btn-primary">Text</button>
+      <button className="btn btn-primary">Play Game</button>
     </div>
   );
 }
