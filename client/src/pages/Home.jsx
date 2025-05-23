4906e9e87bac65555292e5f97ef50f54f66c5714
Prashanna21 <Prashnnanewar21@gmail.com>
2025-05-23 16:27:18 +0545

    setup daisy ui

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-[#70EE9C] h-full">
+      <button className="btn btn-primary">Text</button>
+    </div>
+  );
+}
+
+export default Home;
