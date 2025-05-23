3c1e9ef3ba89e1339c4a4957e2a832685ebb07a3
Pratham943 <phoneixwasfound69@gmail.com>
2025-05-23 19:50:47 +0545

    Card component made for Operatives

--- src/components/TeamSelectionComponent.jsx
+++ src/components/TeamSelectionComponent.jsx
@@ -5,12 +5,12 @@ function TeamSelectionComponent({ team }) {
     team === "red"
       ? "bg-[#1E2F23]"
       : team === "blue"
-      ? "bg-[#70EE9C]"
+      ? "bg-[#34623F]"
       : "bg-gray-200"; // default
 
   return (
     <div
-      className={`${bgColor} w-80 h-52 rounded-2xl shadow-lg p-6 justify-between`}
+      className={`${bgColor} w-80 h-52 rounded-2xl shadow-lg p-6  justify-between`}
     >
       <div className="text-white text-xl font-semibold text-end">-</div>
       <div className="text-white opacity-80 ">Operative(s)</div>
