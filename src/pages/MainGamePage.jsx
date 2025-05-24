9cf068eab98597894e5631bbd112588b53a02cd2
Prashanna21 <Prashnnanewar21@gmail.com>
2025-05-24 08:02:03 +0545

    layout made for both main game and create game pages and added cards

--- src/pages/MainGamePage.jsx
+++ src/pages/MainGamePage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PlayCards from "../components/PlayCards";
+import NavBar from "../components/NavBar";
+import TeamSelectionComponent from "../components/TeamSelectionComponent";
 
 function MainGamePage() {
-  return <div>MainGamePage</div>;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const noOfRows = 5;
+    const noOfCols = 5;
+    const generatedCards = [];
+
+    for (let i = 1; i <= noOfRows; i++) {
+      console.log(i);
+      for (let j = 1; j <= noOfCols; j++) {
+        generatedCards.push(<PlayCards />);
+      }
+    }
+    console.log(generatedCards);
+    setCards(generatedCards);
+  }, []);
+
+  return (
+    <div className="bg-[#79AEA3] h-full px-28 ">
+      <NavBar />
+
+      <div className="flex justify-between mt-12">
+        <TeamSelectionComponent team="blue" />
+        <div className="flex flex-wrap gap-2 items-center justify-center ">
+          {cards.map((card) => card)}
+        </div>
+        <TeamSelectionComponent team="red" />
+      </div>
+    </div>
+  );
 }
 
 export default MainGamePage;
